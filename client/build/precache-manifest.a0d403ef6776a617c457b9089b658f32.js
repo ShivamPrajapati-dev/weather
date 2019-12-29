137def6407ae86247f66ef8594f89cf2cb3f592a
Shivam Prajapati <shivamprajapati.git@gmail.com>
2019-12-30 00:39:26 +0530

    ready to deploy

---
+++ client/build/precache-manifest.a0d403ef6776a617c457b9089b658f32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39421385b249f24703a05a13a1e156a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e94354e6bc0ae6b70fa8",
+    "url": "/static/css/main.7306ce2a.chunk.css"
+  },
+  {
+    "revision": "c00ca21b855f510158ec",
+    "url": "/static/js/2.40cd3eca.chunk.js"
+  },
+  {
+    "revision": "85605b2cae435653bed9f6f28884786e",
+    "url": "/static/js/2.40cd3eca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e94354e6bc0ae6b70fa8",
+    "url": "/static/js/main.526a3d69.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
