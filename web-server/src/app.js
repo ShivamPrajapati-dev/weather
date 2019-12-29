174875c8781548bd91da9162414992fbacbfd4f3
Shivam Prajapati <shivamprajapati.git@gmail.com>
2019-12-30 01:17:09 +0530

    update

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -15,7 +15,7 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, 'client','build')));
+app.use(express.static(path.join(__dirname, '../../client','build')));
 
 
 app.post('/weather',(req,res)=>{
